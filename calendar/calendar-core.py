0475e144fde5be6e42acb1f999119ff780360624
jun <june4991@naver.com>
2025-12-05 22:40:53 +0900

    calendar

--- calendar/calendar-core.py
+++ calendar/calendar-core.py
@@ -8,64 +8,81 @@ import datetime
 주 단위 달력
 '''
 # cli main
-def show_year_calendar(year=None):
+
+
+def get_year_str_calendar(year=None):   
     if year is None:
           year=datetime.datetime.now().year
 
-    text_calendar=calendar.TextCalendar(firstweekday=6)
-    result=text_calendar.formatyear(year)
-        
-    return result
+    str_cal= calendar.TextCalendar(firstweekday=6).formatyear(year)  
+     
+    return str_cal
 
-def show_month_calendar(year=None, month=None):   
-    if year is None or month is None :
-        now=datetime.datetime.now()          #now 를 두번 호출하면 오류 생길수 있음
-        if year is None :
-             year=now.year
-        
-        if month is None:
-            month=now.month
+def get_year_obj_calendar(year=None):
+    if year is None:
+          year=datetime.datetime.now().year
+    obj_cal= calendar.TextCalendar(firstweekday=6)  
+
+    return obj_cal
+
+def get_month_str_calendar(year=None, month=None):   
+    today= datetime.datetime.now()
+    year= year if year is not None else today.year
+    month = month if month is not None else today.month
+    cal_str=calendar.TextCalendar(firstweekday=6).formatmonth(year,month)  
     
-    text_calendar=calendar.TextCalendar(firstweekday=6)
-    result = text_calendar.formatmonth(year,month)
+    return cal_str
 
-    return result
+def get_month_obj_calendar(year=None, month=None):   
+    today= datetime.datetime.now()
+    year= year if year is not None else today.year
+    month = month if month is not None else today.month  
+    cal_obj=calendar.TextCalendar(firstweekday=6)
+
+    return cal_obj
 
 def get_week_calendar(year=None, month=None, day=None):
-    if year is None or month is None or day  is None:
-        now=datetime.datetime.now()
-        if year is None :
-             year=now.year
-        
-        if month is None:
-            month=now.month
-        
-        if day is None:
-            day=now.day 
+    today = datetime.datetime.now()
+    year = year if year is not None else today.year
+    month = month if month is not None else today.month  
+    day = day if day is not None else today.day
     
-    get_date=datetime.date(year,month,day)
-    iso_year, iso_week, iso_weekday = get_date.isocalendar()
-
-    return iso_year,month, iso_week, iso_weekday
-
-def get_day_calendar(year=None, month=None, day=None):
-    if year is None or month is None or day  is None:
-        now=datetime.datetime.now()
-        if year is None :
-             year=now.year
-        
-        if month is None:
-            month=now.month
-        
-        if day is None:
-            day=now.day 
+    d= datetime.date(year,month,day)
+    iso_year, iso_week, iso_weekday = d.isocalendar()
     
-    get_date=datetime.date(year,month,day)
 
-    return get_date
+    return iso_year,d.month, iso_week, iso_weekday
+
+def get_day_str_calendar(year=None, month=None, day=None):
+    
+    today=datetime.datetime.now()
+    year = year if year is not None else today.year
+    month = month if month is not None else today.month
+    day= day if day is not None else today.day
+    date_str=datetime.date(year, month, day).strftime("%Y-%m-%d")
+    
+    
+    return  date_str
+
+def get_day_obj_calendar(year=None, month=None, day=None):
+    
+    today=datetime.datetime.now()
+    year = year if year is not None else today.year
+    month = month if month is not None else today.month
+    day= day if day is not None else today.day
+    date_obj=datetime.date(year, month, day)
+
+    return date_obj
 
         
 
 
 if __name__ == "__main__":  # 메인 파일일 경우 실행_
-     print(get_week_calendar())
+    
+    print(get_year_str_calendar())
+    print(get_year_obj_calendar())
+    print(get_month_str_calendar())
+    print(get_month_obj_calendar())
+    print(get_week_calendar())
+    print(get_day_str_calendar())
+    print(get_day_obj_calendar())
